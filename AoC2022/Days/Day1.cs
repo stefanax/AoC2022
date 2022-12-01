802bd7f0fbe0ab5e72117b870f316b75db7b8457
Stefan Axelsson <stefan.axelsson@bejoynd.com>
2022-12-01 13:05:14 +0100

    Initial commit

---
+++ AoC2022/Days/Day1.cs
@@ -0,0 +1,37 @@
+﻿namespace AoC2022;
+
+public class Day1
+{
+    private InputFiles _inputFiles = new InputFiles();
+
+    public void Step1()
+    {
+        var input = _inputFiles.ReadInputFileForDay(1, false);
+        var inputList = _inputFiles.SplitString(input);
+
+        var maxCalories = 0;
+        var currentCalories = 0;
+
+        foreach (var inputListItem in inputList)
+        {
+            if (inputListItem == "")
+            {
+                if (currentCalories > maxCalories) maxCalories = currentCalories;
+                currentCalories = 0;
+            }
+            else
+            {
+                currentCalories += Int32.Parse(inputListItem);    
+            }
+        }
+        
+        if (currentCalories > maxCalories) maxCalories = currentCalories;  // Just in case the last item is the largest...
+        
+        Console.WriteLine($"Step One Max Cal: {maxCalories}");
+    }
+
+    public void Step2()
+    {
+        
+    }
+}
