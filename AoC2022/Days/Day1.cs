d9e3ff7264056f692082e27ad41651695dba45a8
Stefan Axelsson <stefan.axelsson@bejoynd.com>
2022-12-01 13:16:35 +0100

    Day 1, Step 2

--- AoC2022/Days/Day1.cs
+++ AoC2022/Days/Day1.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC2022;
+﻿using System.Collections;
+
+namespace AoC2022;
 
 public class Day1
 {
@@ -32,6 +34,45 @@ public class Day1
 
     public void Step2()
     {
+        var input = _inputFiles.ReadInputFileForDay(1, false);
+        var inputList = _inputFiles.SplitString(input);
+
+        var maxCalories = new List<int>() { 0, 0, 0 };
+        var currentCalories = 0;
+
+        foreach (var inputListItem in inputList)
+        {
+            if (inputListItem == "")
+            {
+                CheckListValues(maxCalories, currentCalories);
+                currentCalories = 0;
+            }
+            else
+            {
+                currentCalories += Int32.Parse(inputListItem);    
+            }
+        }
         
+        CheckListValues(maxCalories, currentCalories);  // Just in case the last item is the largest...
+
+        var totalMaxCalories = maxCalories[0] + maxCalories[1] + maxCalories[2];
+        Console.WriteLine($"Step Two Total Max Cal: {totalMaxCalories}");
+    }
+
+    public void CheckListValues(List<int> list, int value)
+    {
+        if (value >= list[0])
+        {
+            list[2] = list[1];
+            list[1] = list[0];
+            list[0] = value;
+        } else if (value >= list[1])
+        {
+            list[2] = list[1];
+            list[1] = value;
+        } else if (value >= list[2])
+        {
+            list[2] = value;
+        }
     }
 }
